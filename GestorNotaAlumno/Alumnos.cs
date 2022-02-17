5d15067cf62cc078c8e626a5759f6578bd785c70
MarcosMM13 <mateosmarcosm@gmail.com>
2022-02-17 19:45:43 -0300

    Confirmar proy final

---
+++ GestorNotaAlumno/Alumnos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorNotaAlumno
+{
+   public class Alumnos
+    {
+        public int Id { get; set; }
+        public int Matricula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int CicloLectivo { get; set; }
+        public int NotaParcial1 { get; set; }
+        public int NotaParcial2 { get; set; }
+        public int NotaTrabajoP { get; set; }
+        public string Materia { get; set; }
+    }
+}
